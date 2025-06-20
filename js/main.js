155759be46f3a81932706b46c8710a4c95b97441
TimofeyOrekhov <drexweblancer@yandex.ru>
2025-06-20 12:38:10 +0300

    Добавлен индикатор загрузки и скрытие основного контента во время загрузки комментариев. Обновлен стиль для индикатора загрузки и основного контента.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ import { getComments } from './api.js'
 import { updateComments } from './data.js'
 
 async function initApp() {
+    document.getElementById('loader').style.display = 'block'
+    document.getElementById('main-content').style.display = 'none'
     try {
         const comments = await getComments()
         updateComments(comments)
@@ -11,8 +13,14 @@ async function initApp() {
         addFormListener()
     } catch (error) {
         console.error('Ошибка при инициализации приложения:', error)
+    } finally {
+        document.getElementById('loader').style.display = 'none'
+        document.getElementById('main-content').style.display = 'block'
     }
 }
-initApp()
+
+document.addEventListener('DOMContentLoaded', () => {
+    initApp()
+})
 
 console.log('It works!')
